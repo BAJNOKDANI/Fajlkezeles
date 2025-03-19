6d5e918c403cc8914fc5f394b7ceeb3570d190aa
BAJNOKDANI <szlobodnik.daniel.2022i@bankitatabanya.hu>
2025-03-19 18:28:14 +0100

    Beolvasas kesz

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
+public class Main {
+    static ArrayList<Ember> lista = new ArrayList<>();
+    public static void main(String[] args) {
+        fajlBeOlvasas();
+        for (Ember item : lista){
+            System.out.println(item);
+        }
+    }
+    public static void fajlBeOlvasas(){
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("adatok.txt"));
+            String sor = bufferedReader.readLine();
+            while (sor != null){
+                lista.add(new Ember(sor));
+                sor=bufferedReader.readLine();
+            }
+            bufferedReader.close();
+        }
+        catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 /* *
